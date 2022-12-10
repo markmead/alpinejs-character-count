e172d0bdf441c9c3ed5159a7508e2691b3bc2a0e
Mark Mead <itsmarkmead@gmail.com>
2022-12-10 10:08:22 +0000

    Initial commit

--- builds/cdn.js
+++ builds/cdn.js
@@ -1,5 +1,3 @@
 import count from '../src/index.js'
 
-document.addEventListener('alpine:init', () => {
-  window.Alpine.plugin(count)
-})
+document.addEventListener('alpine:init', () => window.Alpine.plugin(count))
